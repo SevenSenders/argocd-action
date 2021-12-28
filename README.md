28c65b7cea16b9bba613309a65ac8d1db29c2415
Vitaliy Mikeshyn <v.mikeshyn@sevensenders.com>
2021-12-28 21:57:45 +0100

    fix ignore

--- README.md
+++ README.md
@@ -8,10 +8,14 @@ This action deploys the feature environment of your application.
 
 **Required** The branch which will be set as a part of the argocd aplication name.
 
+## `target-commit`
+
+**Required** The commit which will be set as a docker image tag.
+
 ## Outputs
 
 ##
 
 ## Example usage
 
-uses: SevenSenders/argocd-action@master
+uses: SevenSenders/argocd-action@v0.1.0
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { parseArgsStringToArgv } = require('string-argv');
 const yaml = require('js-yaml');
 const { execSync } = require('child_process');
 const core = require('@actions/core');
@@ -11,28 +10,28 @@ function login_to_argocd() {
             ' --password "' + process.env.ARGOCD_PASSWORD + '"'
         execSync(command, {stdio: 'inherit'});
     } catch (error) {
-        process.exit(1)
+        process.exit(1);
     }
 }
 
 function clean_environment_name(name) {
-    const clean_name = name.replace('feature/', '').replace('hotfix/', '').replace('bugfix/', '')
-    return clean_name.replaceAll(/[^a-zA-Z0-9-]+/g, '').replace(/^-+|-+$/g, '').toLowerCase()
+    const clean_name = name.replace('feature/', '').replace('hotfix/', '').replace('bugfix/', '');
+    return clean_name.replaceAll(/[^a-zA-Z0-9-]+/g, '').replace(/^-+|-+$/g, '').toLowerCase();
 }
 
-function create_preview_environment(app_name, env_name) {
-    const preview_app_name = app_name.replace('-dev-', '-' + env_name + '-')
+function create_preview_environment(app_name, env_name, commit_hash) {
+    const preview_app_name = app_name.replace('-dev-', '-' + env_name + '-');
     try {
         const check_exists = 'argocd app get ' + preview_app_name
         execSync(check_exists, { stdio: 'ignore' });
         try {
             const update_image = 'argocd app set ' + preview_app_name +
-                ' --parameter global.image.tag=' + process.env.GITHUB_SHA
+                ' --parameter global.image.tag=' + commit_hash
             execSync(update_image);
-            console.log("The new image: " + process.env.GITHUB_SHA + " was set.");
+            console.log("The new image: " + commit_hash + " was set.");
         } catch (error) {
-            console.log("The new image: " + process.env.GITHUB_SHA + " wasn't set.");
-            process.exit(1)
+            console.log("The new image: " + commit_hash + " wasn't set.");
+            process.exit(1);
         }
         try {
             const wait_operation = 'argocd app wait ' + preview_app_name +
@@ -41,14 +40,14 @@ function create_preview_environment(app_name, env_name) {
             console.log(preview_app_name + " is green.");
         } catch (error) {
             console.log(preview_app_name + " is red. Please check the argocd web interface.");
-            process.exit(1)
+            process.exit(1);
         }
         try {
             const sync = 'argocd app sync ' + preview_app_name
             execSync(sync);
         } catch (error) {
             console.log(" I can't run the sync command. Please check the argocd web interface.");
-            process.exit(1)
+            process.exit(1);
         }
         try {
             const wait_sync = 'argocd app wait ' + preview_app_name +
@@ -57,13 +56,13 @@ function create_preview_environment(app_name, env_name) {
             console.log(preview_app_name + " was synced.");
         } catch (error) {
             console.log(preview_app_name + " wasn't synced. Please check the argocd web interface.");
-            process.exit(1)
+            process.exit(1);
         }
         console.log(preview_app_name + " was deployed.");
     } catch (error) {
         console.log(preview_app_name + " will be created.");
         try {
-            const get_config = 'argocd app get ' + app_name + ' -o yaml'
+            const get_config = 'argocd app get ' + app_name + ' -o yaml';
             const dev_config = execSync(get_config);
             try {
                 const config = yaml.load(dev_config);
@@ -79,70 +78,71 @@ function create_preview_environment(app_name, env_name) {
                     ' --parameter global.pillar=' + config['spec']['source']['helm']['parameters'][0]['value'] +
                     ' --parameter global.serviceName=' + config['spec']['source']['helm']['parameters'][1]['value'] +
                     ' --parameter global.environmentName=' + env_name +
-                    ' --parameter global.image.tag=' + process.env.GITHUB_SHA +
+                    ' --parameter global.image.tag=' + commit_hash +
                     ' --parameter deployment.fullnameOverride=' + preview_app_name +
                     ' --label original=' + app_name +
                     ' --label branch=' + env_name +
                     ' --label environment=preview --label repository=' + process.env.GITHUB_REPOSITORY.replace('SevenSenders/', '') +
                     ' --sync-policy automated --sync-option Prune=true --sync-option CreateNamespace=false --self-heal --upsert'
-                execSync(create_command)
-                console.log(preview_app_name + " was created!")
+                execSync(create_command);
+                console.log(preview_app_name + " was created!");
             } catch (e) {
-                console.log("Failed to deploy application " + app_name + " to Preview environment: " + env_name + "!")
-                process.exit(1)
+                console.log("Failed to deploy application " + app_name + " to Preview environment: " + env_name + "!");
+                process.exit(1);
             }
         } catch (e) {
-            console.log("Failed to get configuration of " + app_name + "!")
-            process.exit(1)
+            console.log("Failed to get configuration of " + app_name + "!");
+            process.exit(1);
         }
     }
 }
 
 function destroy_preview_environment(app_name, env_name) {
-    const preview_app_name = app_name.replace('-dev-', '-' + env_name + '-')
+    const preview_app_name = app_name.replace('-dev-', '-' + env_name + '-');
     try {
         const delete_command = 'argocd app delete ' + preview_app_name
-        execSync(delete_command)
-        console.log(preview_app_name + " was destroyed!")
+        execSync(delete_command);
+        console.log(preview_app_name + " was destroyed!");
     } catch (e) {
-        console.log("Failed to destroy application " + preview_app_name + "!")
-        process.exit(1)
+        console.log("Failed to destroy application " + preview_app_name + "!");
+        process.exit(1);
     }
 }
 
 function destroy_preview_environments(app_name) {
     try {
         const preview_apps_command = 'argocd app list -o name --selector environment=preview --selector original=' + app_name
-        const preview_apps = execSync(preview_apps_command).toString()
+        const preview_apps = execSync(preview_apps_command).toString();
         const list_of_apps = preview_apps.split(/\r?\n/).filter(item => item);
         list_of_apps.forEach(function (app) {
             try {
                 const delete_app = 'argocd app delete ' + app
-                execSync(delete_app)
-                console.log(delete_app + " was deleted.")
+                execSync(delete_app);
+                console.log(delete_app + " was deleted.");
             } catch (e) {
-                console.log("Failed to delete preview environments: " + delete_app + "!")
-                process.exit(1)
+                console.log("Failed to delete preview environments: " + delete_app + "!");
+                process.exit(1);
             }
         });
     } catch (e) {
-        console.log("Failed to list preview environments for " + app_name + "!")
-        process.exit(1)
+        console.log("Failed to list preview environments for " + app_name + "!");
+        process.exit(1);
     }
 }
 
-const env = process.env.ENVIRONMENT_NAME
+const env = process.env.ENVIRONMENT_NAME;
 const branch = core.getInput('target-branch');
-const env_name = clean_environment_name(branch)
-const app_name = [ process.env.TEAM, env, process.env.SERVICE_NAME ].join('-')
+const commit_hash = core.getInput('target-commit');
+const env_name = clean_environment_name(branch);
+const app_name = [ process.env.TEAM, env, process.env.SERVICE_NAME ].join('-');
 login_to_argocd();
-deployment_type = process.env.DEPLOYMENT_TYPE
+deployment_type = process.env.DEPLOYMENT_TYPE;
 if (deployment_type === 'preview' & env === 'dev') {
-    create_preview_environment(app_name, env_name)
+    create_preview_environment(app_name, env_name, commit_hash);
 } else if (deployment_type === 'destroy' & env === 'dev') {
-    destroy_preview_environment(app_name, env_name)
+    destroy_preview_environment(app_name, env_name);
 } else if (deployment_type === 'clean' & env === 'dev') {
-    destroy_preview_environments(app_name)
+    destroy_preview_environments(app_name);
 } else {
-    console.log('DEPLOYMENT_TYPE ' + deployment_type + ' should be one of "promote", "preview", "destroy" or "clean".')
+    console.log('DEPLOYMENT_TYPE ' + deployment_type + ' should be one of "promote", "preview", "destroy" or "clean".');
 }
